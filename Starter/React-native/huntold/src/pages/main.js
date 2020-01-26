444e5b2c208b8ced2f208dbd9141f16c147d64a5
glaulher <glaulher.developer@gmail.com>
2020-01-26 19:08:01 -0300

    react-native with react-navigation

---
+++ Starter/React-native/huntold/src/pages/main.js
@@ -0,0 +1,30 @@
+import React, {Component} from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+export default class Main extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>Bem vindo ao React-native!</Text>
+        <View style={styles.box}></View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  welcome: {
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+  box: {
+    height: 60,
+    width: 60,
+    backgroundColor: '#f00',
+  },
+});
