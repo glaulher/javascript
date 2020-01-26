9c20f5c1d5ce8aff9559a148dedab3a88acdbda6
glaulher <glaulher.developer@gmail.com>
2020-01-26 19:12:07 -0300

    react-native with react-navigation

--- Starter/React-native/huntold/src/routes.js
+++
@@ -1,8 +0,0 @@
-import {createAppContainer} from 'react-navigation';
-import {createStackNavigator} from 'react-navigation-stack';
-import Main from './pages/main';
-
-const Routes = createStackNavigator({
-  Main: Main,
-});
-export default createAppContainer(Routes);
