34183530dcc4bf6320087c1e8dd63bc5adf8faf4
glaulher <glaulher.developer@gmail.com>
2020-02-04 23:18:57 -0300

    WebView update

--- Starter/React-native/hunt/src/routes.js
+++ Starter/React-native/hunt/src/routes.js
@@ -1,8 +1,10 @@
 import {createAppContainer} from 'react-navigation';
 import {createStackNavigator} from 'react-navigation-stack';
 import Main from './pages/main';
+import Product from './pages/product';
 
 const AppNavigator = createStackNavigator({
   Main: Main,
+  Product: Product,
 });
 export default createAppContainer(AppNavigator);
